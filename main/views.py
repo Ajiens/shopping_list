0d76561f9d6f7f4c1ca2d49fc1eecd636cde2e95
alwantakaditama@gmail.com <alwantakaditama@gmail.com>
2023-09-27 16:15:04 +0700

    Tutorial 4 Modif dikit

--- main/views.py
+++ main/views.py
@@ -20,7 +20,7 @@ def show_main(request):
         'name': request.user.username,
         'class': 'PBP E',
         'products': products,
-        'last_login': request.COOKIES['last_login'],
+        'last_login': request.COOKIES.get('last_login', ''),
     }
 
     return render(request, "main.html", context)
